81322b22c286caa032e29de4fe885c074df863df
rafperdevops <rafperdevops@gmail.com>
2024-12-30 15:18:44 -0500

    Ajustes

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -25,7 +25,7 @@ export default {
         {
           id: 1,
           texto:
-            '<strong><em>Un sistema de gestión de mantenimiento se conoce como sistema de información.</em></strong>',
+            '<strong>Un sistema de gestión de mantenimiento se conoce como sistema de información.</strong>',
           imagen: require('@/assets/actividad/pregunta_1.png'),
           barajarRespuestas: true,
           opciones: [
@@ -38,7 +38,7 @@ export default {
         {
           id: 2,
           texto:
-            '<strong><em>Las herramientas tecnológicas aportan desorden en la información.</strong></em>',
+            '<strong>Las herramientas tecnológicas aportan desorden en la información.</strong>',
           imagen: require('@/assets/actividad/pregunta_2.png'),
           barajarRespuestas: true,
           opciones: [
@@ -51,7 +51,7 @@ export default {
         {
           id: 3,
           texto:
-            '<strong><em>Excel es una herramienta que ayuda a la programación y organización de información.</strong></em>',
+            '<strong>Excel es una herramienta que ayuda a la programación y organización de información.</strong>',
           imagen: require('@/assets/actividad/pregunta_3.png'),
           barajarRespuestas: false,
           opciones: [
@@ -64,7 +64,7 @@ export default {
         {
           id: 4,
           texto:
-            '<strong><em>Las tarjetas maestras son fichas donde se consignan las características principales de la máquina.</strong></em>',
+            '<strong>Las tarjetas maestras son fichas donde se consignan las características principales de la máquina.</strong>',
           imagen: require('@/assets/actividad/pregunta_4.png'),
           barajarRespuestas: false,
           opciones: [
@@ -77,7 +77,7 @@ export default {
         {
           id: 5,
           texto:
-            '<strong><em>La fiabilidad es importante para el óptimo funcionamiento de las máquinas.</strong></em>',
+            '<strong>La fiabilidad es importante para el óptimo funcionamiento de las máquinas.</strong>',
           imagen: require('@/assets/actividad/pregunta_1.png'),
           barajarRespuestas: false,
           opciones: [
@@ -90,7 +90,7 @@ export default {
         {
           id: 6,
           texto:
-            '<strong><em>Los requerimientos de una máquina son aquellas acciones de mantenimiento o actividades que se deben practicar a la máquina para que no falle o se dañe.</strong></em>',
+            '<strong>Los requerimientos de una máquina son aquellas acciones de mantenimiento o actividades que se deben practicar a la máquina para que no falle o se dañe.</strong>',
           imagen: require('@/assets/actividad/pregunta_2.png'),
           barajarRespuestas: false,
           opciones: [
@@ -103,7 +103,7 @@ export default {
         {
           id: 7,
           texto:
-            '<strong><em>Luego de realizar la tarjeta maestra, es importante contar para cada máquina con el recuento de identificación del equipo, fecha, código del instructivo, trabajo realizado, costo, tiempo y quien lo realiza.</strong></em>',
+            '<strong>Luego de realizar la tarjeta maestra, es importante contar para cada máquina con el recuento de identificación del equipo, fecha, código del instructivo, trabajo realizado, costo, tiempo y quien lo realiza.</strong>',
           imagen: require('@/assets/actividad/pregunta_3.png'),
           barajarRespuestas: false,
           opciones: [
@@ -116,12 +116,12 @@ export default {
         {
           id: 8,
           texto:
-            '<strong><em>Los tableros auxiliares son tableros utilizados con el fin de sobre escribir el cronograma anual de la asignación de instructivos para cada lote de ganado.</strong></em>',
+            '<strong>Los tableros auxiliares son tableros utilizados con el fin de sobre escribir el cronograma anual de la asignación de instructivos para cada lote de ganado.</strong>',
           imagen: require('@/assets/actividad/pregunta_4.png'),
           barajarRespuestas: false,
           opciones: [
-            { id: 'a', texto: 'Falso', esCorrecta: false },
-            { id: 'b', texto: 'Verdadero', esCorrecta: true },
+            { id: 'a', texto: 'Falso', esCorrecta: true },
+            { id: 'b', texto: 'Verdadero', esCorrecta: false },
           ],
           mensaje_correcto: '¡Muy bien! Ha acertado la respuesta.',
           mensaje_incorrecto: 'Lo sentimos, su respuesta no es la correcta.',
@@ -129,7 +129,7 @@ export default {
         {
           id: 9,
           texto:
-            '<strong><em>La programación y el control de las ordenes de trabajo es importante para el proceso.</strong></em>',
+            '<strong>La programación y el control de las ordenes de trabajo es importante para el proceso.</strong>',
           imagen: require('@/assets/actividad/pregunta_1.png'),
           barajarRespuestas: false,
           opciones: [
@@ -142,7 +142,7 @@ export default {
         {
           id: 10,
           texto:
-            '<strong><em>La fiabilidad, se puede decir que es la probabilidad de que un elemento o equipo trabaje sin fallos durante un tiempo determinado.</strong></em>',
+            '<strong>La fiabilidad, se puede decir que es la probabilidad de que un elemento o equipo trabaje sin fallos durante un tiempo determinado.</strong>',
           imagen: require('@/assets/actividad/pregunta_3.png'),
           barajarRespuestas: false,
           opciones: [
